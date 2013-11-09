7a9c5992eabdecdaf6a0686c1148cecef07ceb9b
Harry Maclean <harryjmaclean@googlemail.com>
2013-11-09 18:25:26 +0000

    Add support for multiple character tokens

--- lisp.rb
+++ lisp.rb
@@ -13,16 +13,20 @@
 # It has a function
 # and one or more arguments
 class Expr
-  def initialize(str)
-    data = str.reject { |c| c == " " }
-    data.shift # First char is (
-    data.pop # Last char is )
-    @func = tokenise(data.shift)
-    @args = data.map { |t| tokenise(t) }
+  def initialize(tokens)
+    tokens.shift # First token is (
+    tokens.pop # Last token is )
+    @func = tokenise(tokens.shift)
+    @args = tokens.map { |t| tokenise(t) }
   end
 
   def call(*args)
-    if @func.arity == @args.count
+    # A negative arity implies an optional number of arguments (i.e. a splat)
+    # -1 => 0+ arguments
+    # -2 => 1+ arguments
+    # etc.
+    n = @func.arity.abs - 1
+    if @func.arity == @args.count or (@func.arity < 0 and @args.count > n)
       @args.map! { |a| a.is_a?(Expr) ? a.call : a }
       @func.call *@args
     else
@@ -37,19 +41,11 @@ class Expr
 end
 
 def parse_expr(str)
-  _str = str
-  while _str.index(")") != nil
+  _str = str_to_array(str)
+  while _str.length > 1
     _str = parse(_str)
   end
   _str.first
-end
-
-def parse(str)
-  str = str.split("") unless str.is_a? Array
-  close = str.index ")"
-  open = close - str[0..close].reverse.index("(")
-  expr_str = str[open..close]
-  new_str = str[0...open] + [Expr.new(expr_str)] + str[close+1..str.length-1]
 end
 
 def tokenise(t)
@@ -59,14 +55,26 @@ def tokenise(t)
   end
   case t
   when "+"
-    lambda { |a, b| a+b }
+    Proc.new { |a, b| a+b }
   when "-"
-    lambda { |a, b| a-b }
+    Proc.new { |a, b| a-b }
   when "*"
-    lambda { |a, b| a*b }
+    Proc.new { |a, b| a*b }
   when "/"
-    lambda { |a, b| a/b }
+    Proc.new { |a, b| a/b }
   when "first"
-    lambda { |a, *b| a }
+    Proc.new { |a, *b| a }
   end
+end
+
+def str_to_array(str)
+  str.gsub(/([()])/, ' \1 ')  # Expand brackets so all tokens are separated by spaces
+    .split " "                # Convert string to array of tokens
+end
+
+def parse(arr)
+  close = arr.index ")"
+  open = close - arr[0..close].reverse.index("(")
+  expr_tokens = arr [open..close]
+  arr[0...open] + [Expr.new(expr_tokens)] + arr[close+1..arr.length-1]
 end
