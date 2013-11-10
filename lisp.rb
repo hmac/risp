1849b9beecf692d38229d8895dc5b54b69faa3f2
Harry Maclean <harryjmaclean@googlemail.com>
2013-11-10 14:40:00 +0000

    Add support for lists

--- lisp.rb
+++ lisp.rb
@@ -1,10 +1,19 @@
 # Lisp code is nested S-expressions
 # An S-expression is an opening bracket, a function name, 1 or more arguments and a closing bracket
-# Arguments can be literals, variables, functions or S-expressions
+# Arguments can be literals, variables, functions, S-expressions or lists
 # (+ 1 2) 
 # => 3
 # (+ (+ 1 2) 3) 
 # => 6
+
+# There are also lists
+# Lists are like S-expressions without the function
+# They are just a list of literals, variables, functions, lists or S-expressions
+# Lists are denoted with a ' before the (
+# '(1 2 3 4)
+# Example usage:
+# (first '(1 2 3 4)) => 1
+# (map (+ 5) '(1 2 3)) => (6 7 8)
 
 # We need a parser to read the program as text and covert the text to some sort of 
 # syntax tree
@@ -14,10 +23,11 @@
 # and one or more arguments
 class Expr
   def initialize(tokens)
-    tokens.shift # First token is (
-    tokens.pop # Last token is )
-    @func = tokenise(tokens.shift)
-    @args = tokens.map { |t| tokenise(t) }
+    # First token is (
+    # Last token is )
+    tokens = tokens[1..-2]
+    @func = tokenise(tokens[0])
+    @args = tokens.drop(1).map { |t| tokenise(t) }
   end
 
   def call(*args)
@@ -49,7 +59,7 @@ def parse_expr(str)
 end
 
 def tokenise(t)
-  return t if t.is_a? Expr
+  return t if t.is_a? Expr or t.is_a? Array
   if t.to_i.to_s == t
     return t.to_i
   end
@@ -63,20 +73,43 @@ def tokenise(t)
   when "/"
     Proc.new { |a, b| a/b }
   when "first"
-    Proc.new { |a, *b| a }
-  when "last"
-    Proc.new { |a, *b| b }
+    Proc.new { |arr| arr.first }
+  when "rest"
+    Proc.new { |arr| b[0..b.length-1] }
   end
+end
+
+def list(tokens)
+  # First token is '(
+  # Last token is )
+  tokens[1..-2].map { |e| tokenise e }
 end
 
 def str_to_array(str)
   str.gsub(/([()])/, ' \1 ')  # Expand brackets so all tokens are separated by spaces
+    .gsub("' (", "'(")        # Join ' ( together into one '( token
     .split " "                # Convert string to array of tokens
 end
 
 def parse(arr)
-  close = arr.index ")"
-  open = close - arr[0..close].reverse.index("(")
-  expr_tokens = arr [open..close]
-  arr[0...open] + [Expr.new(expr_tokens)] + arr[close+1..arr.length-1]
+  close = arr.index ")" # Find first closing bracket
+  # Find corresponding opening bracket
+  first_expr_bracket = arr[0..close].reverse.index("(")
+  first_list_bracket = arr[0..close].reverse.index("'(") || arr.length
+  if first_list_bracket < first_expr_bracket
+    # We've found a list
+    open = close - first_list_bracket
+    list_tokens = arr[open..close]
+    arr[0...open] + [list(list_tokens)] + arr[close+1..-1]
+  else
+    # We've found an expression
+    open = close - first_expr_bracket
+    expr_tokens = arr[open..close] # Get tokens inside these two brackets
+    arr[0...open] + [Expr.new(expr_tokens)] + arr[close+1..arr.length-1] # Convert tokens to an expression
+  end
+end
+
+# Convenience eval method
+def run(code)
+  parse_expr(code).call
 end
