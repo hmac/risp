be87da44a9e9becaeb4e1efe4585ca84db0d8135
Harry Maclean <harryjmaclean@googlemail.com>
2013-11-21 21:10:04 +0000

    Fix bugs in python port.

--- pisp.py
+++ pisp.py
@@ -1,13 +1,18 @@
 import re
 
+def run(code):
+	node = parse_exp(code)
+	return node.call().value
+
 def parse_exp(str):
 	arr = str_to_array(str)
 	node = parse(arr)
+	return node
 
 def tokenise(t, node):
 	if type(t) == list:
 		return t
-	if str(int(t)) == t:
+	if is_number(t):
 		return Literal(int(t))
 	if t == "+":
 		return lambda a,b: Literal(a.value+b.value)
@@ -19,6 +24,13 @@ def tokenise(t, node):
 			return res
 		return Literal(t)
 
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
 
 def str_to_array(str):
 	str = re.sub("\(", " ( ", str)
@@ -26,21 +38,22 @@ def str_to_array(str):
 	return str.split()
 
 def parse(arr):
-	cur_node = global_root
+	global root
+	cur_node = root
 	for token in arr:
 		if token == "(":
-			cur_node.push Node(nil,cur_node)
+			cur_node.push(Node(None,cur_node))
 			cur_node = cur_node.children[-1]
 		elif token == ")":
 			cur_node = cur_node.parent
 		else:
-			cur_node.push Node.new(tokenise(token, cur_node), cur_node)
-	return global_root
+			cur_node.push(Node(tokenise(token, cur_node), cur_node))
+	return root.children[-1]
 
 class Literal:
 	"""its a literal"""
-	def __init__(self, val):
-		self.val = val
+	def __init__(self, value):
+		self.value = value
 
 class Node:
 	"""issa node"""
@@ -49,34 +62,35 @@ class Node:
 		self.parent = parent
 		self.children = []
 		self.context = {}
-	def root:
-		if self.parent == nil:
+
+	def root(self):
+		if self.parent == None:
 			return self
 		else:
-			return self.parent.root
-	def push(node):
+			return self.parent.root()
+	def push(self, node):
 		self.children.append(node)
-	def resolve(name):
+	def resolve(self, name):
 		if name in self.context.keys():
 			return self.context[name]
-		if !self.parent:
-			return nil
+		if not self.parent:
+			return None
 		return self.parent.resolve(name)
-	def hoist(name, val):
+	def hoist(self, name, val):
 		self.context[name] = val
-	def to_a:
+	def to_a():
 		if len(self.children) == 0:
 			return self.value
-		else
-			map(lambda c: c.to_a, self.children)
-	def call:
-		if len(self.children) == 0:
-			return self.value
-		if type(self.children[0]value) == type(lambda: None):
-			self.children[0].value.call *map(lambda c: c.value || c.call,self.children[1:])
 		else:
-			map(lambda c: c.value if c.value else c.call, self.children)
+			map(lambda c: c.to_a, self.children)
+	def call(self):
+		if len(self.children) == 0:
+			return self.value
+		if type(self.children[0].value) == type(lambda: None):
+			return self.children[0].value(*map(lambda c: c.value if c.value else c.call(),self.children[1:]))
+		else:
+			return map(lambda c: c.value if c.value else c.call(), self.children)
 
-global global_root = Node(nil,nil)
+root = Node(None,None)
 		
 
