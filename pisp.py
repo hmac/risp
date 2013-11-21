76901652391a55a63e507c780a77ac52d1f42d9e
Harry Maclean <harryjmaclean@googlemail.com>
2013-11-21 22:09:30 +0000

    Add support for 'let'. Pisp has now significantly diverged from Risp.

--- pisp.py
+++ pisp.py
@@ -13,15 +13,15 @@ def tokenise(t, node):
 	if type(t) == list:
 		return t
 	if is_number(t):
-		return Literal(int(t))
+		return Literal(int(t), node)
 	if t == "+":
-		return lambda a,b: Literal(a.value+b.value)
+		return lambda a,b: Literal(a.val()+b.val(), node)
 	elif t == "-":
-		return lambda a,b: Literal(a.value-b.value)
+		return lambda a,b: Literal(a.val()-b.val(), node)
 	elif t == "*":
-		return lambda a,b: Literal(a.value*b.value)
+		return lambda a,b: Literal(a.val()*b.val(), node)
 	elif t == "/":
-		return lambda a,b: Literal(a.value/b.value)
+		return lambda a,b: Literal(a.val()/b.val(), node)
 	elif t == "first":
 		return lambda arr: arr[0]
 	elif t == "last":
@@ -29,7 +29,7 @@ def tokenise(t, node):
 	elif t == "rest":
 		return lambda arr: arr[1:]
 	elif t == "def":
-		return lambda name,val: node.root().hoist(name.value, val)
+		return lambda name,val: node.root().hoist(name.val(), val)
 	elif t == "let":
 		# The way S-expressions are currently evaluated
 		# (from the inside out) means that by the time this
@@ -37,22 +37,21 @@ def tokenise(t, node):
 		# define have already been used (or attemped to be used)
 		# and so an error has probably already occurred as they will
 		# have been treated as strings.
-		# We'd have to somehow detect when we come across a 'let'
-		# function in the Node.call method so that we don't try to
-		# evaluate the contained expression before the variables have been set.
-		pass
+		# So we have to treat let differently, and handle it during Node.call
+		# when the 2nd argument is yet to be evaluated.
+		return let
 	else:
-		res = node.resolve(t)
-		if res:
-			return res
-		return Literal(t)
+		return Literal(t, node)
+
+def let(node,binding):
+	node.hoist(binding[0].val(), binding[1].val())
 
 def is_number(s):
-    try:
-        float(s)
-        return True
-    except ValueError:
-        return False
+  try:
+    float(s)
+    return True
+  except ValueError:
+    return False
 
 
 def str_to_array(str):
@@ -75,8 +74,25 @@ def parse(arr):
 
 class Literal:
 	"""its a literal"""
-	def __init__(self, value):
+	def __init__(self, value, node):
 		self.value = value
+		self.node = node
+		self.resolved = False
+	def val(self):
+		if self.resolved:
+			return self.value
+		if is_number(self.value):
+			self.resolved = True
+			self.value = float(self.value)
+			return self.value
+		res = self.node.resolve(self.value)
+		if res:
+			self.resolved = True
+			self.value = res
+			return self.value
+		else:
+			# Should probably raise an error here
+			return self.value
 
 class Node:
 	"""issa node"""
@@ -105,6 +121,10 @@ class Node:
 		if len(self.children) == 0:
 			return self.value
 		if type(self.children[0].value) == type(lambda: None):
+			# Special case for 'let'
+			if self.children[0].value == let:
+				let(self, self.children[1].call())
+				return self.children[2].call()
 			return self.children[0].value(*map(lambda c: c.value if c.value else c.call(),self.children[1:]))
 		else:
 			return map(lambda c: c.value if c.value else c.call(), self.children)
